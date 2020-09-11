d9e08070ecb9c810bb40e61b7d23f549e7a0d197
emmeem <1124782639@qq.com>
2020-09-11 21:48:50 +0800

    finish problem2

--- src/main/java/com/thoughtworks/capability/gtb/serializer/PersonVoSerializer.java
+++ src/main/java/com/thoughtworks/capability/gtb/serializer/PersonVoSerializer.java
@@ -10,16 +10,25 @@ import com.thoughtworks.capability.gtb.vo.PersonVo;
 import java.io.IOException;
 
 
-public class AgeSerializer extends StdSerializer<PersonVo> {
-    protected AgeSerializer() {
+public class PersonVoSerializer extends StdSerializer<PersonVo> {
+    protected PersonVoSerializer() {
         super(PersonVo.class);
     }
 
     @Override
     public void serialize(PersonVo person, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeStartObject();
 
+        gen.writeStringField("id", person.getId());
         if(person.getAge() == null) {
-            person.setAge(0);
+            gen.writeNumberField("age",0);
+        }else {
+            gen.writeNumberField("age",person.getAge());
         }
+        gen.writeStringField("name", person.getName());
+        if(person.getHobby() != null) {
+            gen.writeStringField("hobby", person.getHobby());
+        }
+        gen.writeEndObject();
     }
 }
