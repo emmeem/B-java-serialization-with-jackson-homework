fbfc2574a4f2e27185f2c16e1c8167a2e661ba9b
emmeem <1124782639@qq.com>
2020-09-11 19:52:32 +0800

    receive

--- src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
+++ src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
@@ -24,6 +24,7 @@ public class EventController {
 
   @PostMapping("/events")
   public void createEvent(@RequestBody EventVo event) {
+
     log.info("create event: {}", event);
   }
 }
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,12 @@ public class EventVo {
 
   private String id;
   private String name;
+
   private EventType type;
+
+  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
   private Date time;
+
+  @JsonUnwrapped
   private UserVo user;
 }
